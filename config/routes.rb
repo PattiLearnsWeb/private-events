69676412feba0b5f52a63331bf592fd29d4d3d5b
Deutro <patrick.engelkes@gmail.com>
2015-06-19 11:13:54 +0200

    Enable user login.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   get 'about' => 'static_pages#about'
+  
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 end
